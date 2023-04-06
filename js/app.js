44d2016de30a9a1f56c8c2eeb4408c939dc4ce03
Sirojiddin Turopov <sirojiddinturopov707@gmail.com>
2023-04-06 12:15:24 +0500

    Owl Carousel Tayor

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
